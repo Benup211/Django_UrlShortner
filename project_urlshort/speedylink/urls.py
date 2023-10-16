8026e03870c474ee0e4e5088b319ca63ec36e3cf
benup211 <benup211@gmail.com>
2023-10-16 20:02:20 +0545

    changes:css

--- project_urlshort/speedylink/urls.py
+++ project_urlshort/speedylink/urls.py
@@ -3,4 +3,7 @@ from . import views
 app_name="speedylink"
 urlpatterns=[
     path('',views.index,name="index"),
+    path('profile/',views.profile,name="profile"),
+    path('Signin/',views.Signin_view,name="signin"),
+    path('Signup/',views.Signup_view,name="signup"),
 ]
--- project_urlshort/speedylink/views.py
+++ project_urlshort/speedylink/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     context={}
     return render(request,'speedylink/index.html',context)
+
+def profile(request):
+    if request.user.is_anonymous:
+        return redirect('speedylink:signin')
+    else:
+        return HttpResponse("user is online")
+
+def Signin_view(request):
+    if request.user.is_anonymous:
+        return render(request,'speedylink/signin.html',{})
+    else:
+        return redirect('speedylink:profile')
+
+def Signup_view(request):
+    return HttpResponse("SignUp")
